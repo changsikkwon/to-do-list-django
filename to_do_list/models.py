8872ddc2ce0c38784e3d7f277bcff1526cc1796d
chang sik kwon <kcs1987@gamil.com>
2021-02-03 22:20:32 +0900

    to_do_list_graphql

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -1,9 +1,27 @@
-from django import db
 from django.db import models
 
 class User(models.Model):
   account = models.CharField(max_length=32)
   password = models.CharField(max_length=32)
+  created_at = models.DateTimeField(auto_now_add = True)
+  updated_at = models.DateTimeField(auto_now = True)
 
   class Meta:
     db_table = "user"
+    
+class Status(models.Model):
+  status_name = models.CharField(max_length=32)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  
+  class Meta:
+    db_table = "status"
+    
+class ToDo(models.Model):
+  title = models.CharField(max_length=32)
+  content = models.TextField()
+  status = models.ForeignKey(Status, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add = True)
+  updated_at = models.DateTimeField(auto_now = True)
+  
+  class Meta:
+    db_table = "to_do"
